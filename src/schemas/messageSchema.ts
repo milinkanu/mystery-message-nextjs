9fdac1430852eaf138b5ceffeb86708a1869677d
milinkanu <milinrkanu999@gmail.com>
2026-01-06 23:56:48 +0530

    feat: Initialize Next.js application with user authentication, messaging schemas, and MongoDB integration.

---
+++ src/schemas/messageSchema.ts
@@ -0,0 +1,8 @@
+import { z } from "zod";
+
+export const messageSchema = z.object({
+    content: z
+    .string()
+    .min(1, {message: "Message must be at least 1 character long"})
+    .max(300, {message: "Message must be at most 300 characters long"})
+})
