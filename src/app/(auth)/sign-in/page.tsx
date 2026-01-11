392346e9f150b48ecd4cbcf1c3c729a441443a1f
milinkanu <milinrkanu999@gmail.com>
2026-01-11 20:34:01 +0530

    Designed signup, signin and verify code ui and connected it with backend endpoints

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,20 +1,107 @@
 'use client'
-import { useSession, signIn, signOut } from "next-auth/react"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import * as z from "zod"
+import Link from "next/link"
+import { useState } from "react"
+import { toast } from "sonner"
+import { useRouter } from "next/navigation"
+import { signInSchema } from "@/schemas/signInSchema"
+import { signIn } from "next-auth/react"
+import { Form, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { Button } from "@/components/ui/button"
 
-export default function Component() {
-  const { data: session } = useSession()
-  if (session) {
-    return (
-      <>
-        Signed in as {session.user?.email} <br />
-        <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => signOut()}>Sign out</button>
-      </>
-    )
+
+const SignInPage = () => {
+  const [isSubmitting, setIsSubmitting] = useState(false)
+  const [username, setUsername] = useState('')
+  const router = useRouter()
+
+  //zod implementation
+  const form = useForm<z.infer<typeof signInSchema>>({
+    resolver: zodResolver(signInSchema),
+    defaultValues: {
+      identifier: '',
+      password: '',
+    }
+  })
+
+  const onSubmit = async (data: z.infer<typeof signInSchema>) => {
+    setIsSubmitting(true)
+    const result = await signIn('credentials', {
+      redirect: false,
+      identifier: data.identifier,
+      password: data.password
+    })
+
+    if (result?.error) {
+      if (result.error === 'CredentialsSignin') {
+        toast.error("Incorrect username or password")
+      } else {
+        toast.error(result.error)
+      }
+    }
+
+    if (result?.url) {
+      toast.success("Login successful")
+      router.replace('/dashboard')
+    }
+    setIsSubmitting(false)
   }
+
   return (
-    <>
-      Not signed in <br />
-      <button className="bg-blue-500 text-white px-4 py-2 rounded" onClick={() => signIn()}>Sign in</button>
-    </>
+    <div className="flex justify-center items-center min-h-screen bg-gray-800">
+      <div className="w-full max-w-md p-8 space-y-8 bg-white rounded-lg shadow-md">
+        <div className="text-center">
+          <h1 className="text-2xl font-extrabold tracking-tight lg:text-3xl mb-6">
+            Welcome Back to True Feedback
+          </h1>
+          <p className="mb-4">Sign in to continue your secret conversations</p>
+        </div>
+        <Form {...form}>
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+            <FormField
+              name="identifier"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Username</FormLabel>
+                  <Input placeholder="Username" {...field} onChange={(e) => {
+                    field.onChange(e)
+                    setUsername(e.target.value)
+                  }} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="password"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem> 
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" {...field} placeholder="Password"/>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <Button className='w-full' type="submit" disabled={isSubmitting}>
+              {isSubmitting ? 'Signing In...' : 'Sign In'}
+            </Button>
+          </form>
+        </Form>
+        <div className="text-center mt-4">
+          <p>
+            Not a member yet?{' '}
+            <Link href="/sign-up" className="text-blue-600 hover:text-blue-800">
+              Sign up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
   )
 }
+
+export default SignInPage
